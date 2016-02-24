f73a85f20233fe07c3e5ca33710fcbad2d13a38c
Egor Manzhula <egor.manzhula@kreditech.com>
2016-02-24 11:02:43 +0200

    Update lecture 2

--- lectures/2. Lecture/2. Typecasting/4. Objects/script.js
+++ lectures/2. Lecture/2. Typecasting/4. Objects/script.js
@@ -3,10 +3,21 @@ console.log('\nOBJECTS');
 /**
  * Objects
  */
-var firstObject = {};
-var secondObject = {};
+var firstObject = {
+    toString: function() {
+        return '5';
+    }
+};
+
+var secondObject = {
+    toString: function() {
+        return '10';
+    }
+};
 
 console.log('Sum objects', firstObject + secondObject);
+console.log('Sum objects', firstObject.toString() + secondObject.toString());
+
 
 console.log('Subtract objects', firstObject - secondObject);
 
@@ -34,7 +45,7 @@ firstObject = {
         return 10;
     },
     toString: function() {
-        return 'firstObject'
+        return 'firstObject';
     }
 };
 
@@ -64,3 +75,34 @@ console.log('Less than boolean', firstObject < secondObject);
 console.log('Greater than or equal', firstObject >= secondObject);
 
 console.log('Less than  or equal', firstObject <= secondObject);
+
+
+var myObj = {
+    valueOf: function () {
+        console.log('FIRST');
+        return this; // Not implemented
+    },
+
+    toString: function () {
+        console.log('SECOND');
+        return 'THERE IS NOTHING TO DO HERE!';
+    }
+};
+
+var myObj2 = {
+    valueOf: function () {
+        console.log('FIRST');
+
+        return this;
+    },
+
+    toString: function () {
+        console.log('SECOND');
+        //return 'THERE IS NOTHING TO DO HERE!';
+
+        return this;// [Object object]
+    }
+};
+
+var a = 2; // Number literal
+var s = 'str'; //
