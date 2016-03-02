e9a12bc28dd939fc551a2920e28456f2ff470f52
Egor Manzhula <egor.manzhula@kreditech.com>
2016-03-02 11:30:41 +0200

    Update array example

--- lectures/3. Lecture/2. Arrays/script.js
+++ lectures/3. Lecture/2. Arrays/script.js
@@ -38,8 +38,41 @@ colors[colors.length] = 'magenta';
 /**
  * or just using methods
  */
-colors.push('cyan'); // adds to the end
+colors.push('cyan', 'pink'); // adds to the end
 colors.unshift('black', 'gray'); // adds to start
 
+/**
+ * Pay attention
+ * There is no associative arrays
+ * this will add property to array object
+ */
+colors['test'] = 'Become property';
+/**
+ * Equal to
+ */
+colors.test = 'I\'ll become property';
 
-console.log('And now colors become = ', colors, ' length = ', colors.length); //
+console.log('And now first colors become = ', colors, 'length is = ', colors.length);
+console.log('Colors = ', colors.test);
+
+console.log('And now colors become = ', colors, ' length = ', colors.length);
+
+var last  = colors.pop(); // Modify array
+console.log('Last color is = ', last, 'and colors become = ', colors, 'length is =', colors.length);
+
+
+var first = colors.shift(); // Modify array
+console.log('Last color is = ', first, 'and colors become = ', colors, 'length is =', colors.length);
+
+/**
+ * Pay attention
+ * elements that doesn't exists will be undefined
+ */
+colors[20] = 'white';
+console.log('Colors become = ', colors, 'length is =', colors.length);
+
+
+console.log('Index of black =', colors.indexOf('magenta'));
+console.log('Index of non existing color =', colors.indexOf('navy'));
+
+console.log('Join array to string = ', colors.join(';'));
